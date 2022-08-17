53f0a280a9b1de2181d286cba145794176e7bc68
Mohd Taha Sharjeel <63963692+tahasharjeel@users.noreply.github.com>
2022-08-18 01:28:23 +0530

    Updated code to extract count from both cmd 9 & 10

--- script.js
+++ script.js
@@ -1,93 +1,75 @@
 function myFunction(){
-let map = new Map();
-let text = document.getElementById("floatingTextarea").value;
 
-let count = 0;
+  let map = new Map();
+  let text = document.getElementById("floatingTextarea").value;
+  let textcmd9 = document.getElementById("floatingTextarea2").value;
 
-let textArray = text.split(/[\n\r]+/);
+  if(text != "" && textcmd9 != ""){
+    let count = 0;
+    let textArray = text.split(/[\n\r]+/);
 
-for(let i=0;i<textArray.length;i++){
-  let line = textArray[i];
-  if(line.includes("distinctMessagesCount") && line.includes("unAcknowledgedCount") && count<24){
-    let start = line.indexOf("distinctMessagesCount");
-    let end = line.indexOf("unAcknowledgedCount") - 2;
-	  let ending = line.substring(start+23,end);
-    //csvArray.push(count+","+ending+"\n");
-	  map.set(count++,ending);
+    for(let j=0;j<textArray.length;j++){
+      let line = textArray[j];
+      if(line.includes("distinctMessagesCount") && line.includes("unAcknowledgedCount") && count<24){
+        let start = line.indexOf("distinctMessagesCount");
+        let end = line.indexOf("unAcknowledgedCount") - 2;
+	      let ending = line.substring(start+23,end);
+	      map.set(count++,ending);
+      }
+    }
+
+    let start2 = textcmd9.indexOf("distinctMessagesCount");
+    let end2 = textcmd9.indexOf("unAcknowledgedCount") - 2;
+	  let ending2 = textcmd9.substring(start2+23,end2);
+
+	  map.set(9,ending2);
+    map.set(23,"Replace from cmd 9");
+
+    let table = document.getElementById("myTable");
+
+    for(let i=1;i<=24;i++){
+      table.rows[i].cells[1].innerHTML = map.get(i-1);
+    }
+
+    document.getElementById("dld").disabled = false;
+
   }
-}
-
-map.set(9,"Replace from cmd 9");
-map.set(23,"Replace from cmd 9");
-
-let table = document.getElementById("myTable");
-
-for(let i=1;i<=24;i++){
-  table.rows[i].cells[1].innerHTML = map.get(i-1);
-}
-
-console.log(map);
-//tableToCSV();
-document.getElementById("dld").disabled = false;
+  else{
+    alert("Please enter both cmd 9 and cmd 10");
+  }
 }
 
 function tableToCSV() {
 
-    // Variable to store the final csv data
-    var csv_data = [];
+  var csv_data = [];
+  var rows = document.getElementsByTagName('tr');
 
-    // Get each row data
-    var rows = document.getElementsByTagName('tr');
-    for (var i = 0; i < rows.length; i++) {
-
-        // Get each column data
+  for(var i = 0; i < rows.length; i++){
         var cols = rows[i].querySelectorAll('td,th');
-
-        // Stores each csv row data
         var csvrow = [];
-        for (var j = 0; j < cols.length; j++) {
-
-            // Get the text data of each cell of
-            // a row and push it to csvrow
-            csvrow.push(cols[j].innerHTML);
+        for(var j = 0; j < cols.length; j++){
+          csvrow.push(cols[j].innerHTML);
         }
-
-        // Combine each column value with comma
         csv_data.push(csvrow.join(","));
-    }
-    // combine each row data with new line character
-    csv_data = csv_data.join('\n');
+  }
 
-    /* We will use this function later to download
-    the data in a csv file downloadCSVFile(csv_data);
-    */
-    downloadCSVFile(csv_data);
+  csv_data = csv_data.join('\n');
+
+  downloadCSVFile(csv_data);
 }
 
+function downloadCSVFile(csv_data){
 
-function downloadCSVFile(csv_data) {
+  CSVFile = new Blob([csv_data], {
+    type: "text/csv"
+  });
 
-           // Create CSV file object and feed
-           // our csv_data into it
-           CSVFile = new Blob([csv_data], {
-               type: "text/csv"
-           });
-
-           // Create to temporary link to initiate
-           // download process
-           var temp_link = document.createElement('a');
-
-           // Download csv file
-           temp_link.download = "DatabricksDistinctMessagesCount.csv";
-           var url = window.URL.createObjectURL(CSVFile);
-           temp_link.href = url;
-
-           // This link should not be displayed
-           temp_link.style.display = "none";
-           document.body.appendChild(temp_link);
-
-           // Automatically click the link to
-           // trigger download
-           temp_link.click();
-           document.body.removeChild(temp_link);
-       }
+  var temp_link = document.createElement('a');
+  temp_link.download = "DatabricksDistinctMessagesCount.csv";
+  var url = window.URL.createObjectURL(CSVFile);
+  temp_link.href = url;
+  temp_link.style.display = "none";
+  document.body.appendChild(temp_link);
+  temp_link.click();
+  document.body.removeChild(temp_link);
+}
